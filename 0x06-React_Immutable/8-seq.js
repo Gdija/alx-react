ccb4d17d72550d7605d868aedc737eb9551c461d
Gdija <khadijaghadi00@gmail.com>
2024-04-25 16:40:51 +0100

    lazyseq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -9,7 +9,7 @@ export default function printBestStudents(object) {
     .map((student) => ({
         score: student.score,
         firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
-        lastName: student.lastNameName.charAt(0).toUpperCase() + student.lastName.slice(1),
+        lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
 
     }));
     console.log(lazySeq.toJS());
