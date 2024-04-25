c8d6d308f75bc2eec45f455cfdc49342f5132e68
Gdija <khadijaghadi00@gmail.com>
2024-04-25 11:13:26 +0100

    fix error

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,6 +1,6 @@
 // Converting into an Immutable object using fromJS
 
-import { fromJS } from "./node_modules/immutable/dist/immutable";
+import { fromJS } from './node_modules/immutable/dist/immutable';
 
 const getImmutableObject = (object) => fromJS(object);
 
