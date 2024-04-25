d000dc7b2528597085f0e2523901940262a0a927
Gdija <khadijaghadi00@gmail.com>
2024-04-25 13:13:53 +0100

    fix err

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
 import { fromJS } from './node_modules/immutable/dist/immutable';
 
 export default function accessImmutableObject(object, array) {
+
     const immutableObj = fromJS(object);
-    const firstName = immutableObj.getIn(array, undefined);
-    return firstName;
+    return immutableObj.getIn(array, undefined);
 }
