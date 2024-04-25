3ba0b2dc574013ed63c875be2dc85c08ec3f68f4
Gdija <khadijaghadi00@gmail.com>
2024-04-25 11:40:21 +0100

    map

---
+++ 0x06-React_Immutable/1-map.js
@@ -0,0 +1,7 @@
+// Converting into Immutable using Map
+
+import { Map } from './node_modules/immutable/dist/immutable';
+
+const getImmutableObject = (object) => Map(object);
+
+export default getImmutableObject;
