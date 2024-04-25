e787b7d0d88535c362658d1533930ebe895caa8e
Gdija <khadijaghadi00@gmail.com>
2024-04-25 15:44:31 +0100

    fix err

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -2,7 +2,7 @@
 
 import { Map } from 'immutable';
 
-export function mergeDeeplyElements(page1, page2){
+export default function mergeDeeplyElements(page1, page2){
     const obj1 = Map(page1);
     const obj2 = Map(page2);
     return obj1.mergeDeep(obj2);
