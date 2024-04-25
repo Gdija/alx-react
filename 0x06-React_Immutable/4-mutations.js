df4135a8cbc937ee2e7e2250d2f2b7bce7d45919
Gdija <khadijaghadi00@gmail.com>
2024-04-25 14:31:21 +0100

    mutation

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -3,5 +3,5 @@
 import { Map } from './node_modules/immutable/dist/immutable';
 
 export const map = Map({1: 'Liam', 2: 'Noah', 3: 'Elijah', 4: 'Oliver', 5: 'Jacob', 6: 'Lucas',});
-export const map2 = map.withMutations(function(map){
-    map.set('2', 'Benjamin').set('4', 'Oliver')});
+export const map2 = map.withMutations(function(map1){
+    map1.set('2', 'Benjamin').set('4', 'Oliver')});
