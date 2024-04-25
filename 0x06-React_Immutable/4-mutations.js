c2349e246c656cab490ce6c357d4e1b7fe6e2327
Gdija <khadijaghadi00@gmail.com>
2024-04-25 14:25:46 +0100

    mutation

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,7 @@
+// chained mutations
+
+import { Map } from './node_modules/immutable/dist/immutable';
+
+export const map = Map({1: 'Liam', 2: 'Noah', 3: 'Elijah', 4: 'Oliver', 5: 'Jacob', 6: 'Lucas',});
+export const map2 = map.withMutations(function(map){
+    map.set('2', 'Benjamin').set('4', 'Oliver')});
