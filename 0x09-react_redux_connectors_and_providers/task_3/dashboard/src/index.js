10436c678c34e2ebd8fbccaea06e8441d49b09d5
Gdija <khadijaghadi00@gmail.com>
2024-05-16 00:30:15 +0100

    devtools

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -7,8 +7,9 @@ import {Provider} from 'react-redux' ;
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-
-const store = createStore(uiReducer, composeWithDevTools(applyMiddleware(thunk)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ 
+const store = createStore(uiReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
