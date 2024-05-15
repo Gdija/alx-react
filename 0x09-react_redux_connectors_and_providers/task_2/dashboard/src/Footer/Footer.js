80fbecb3c47f4e4a82e0a1ec72346dd40485353b
Gdija <khadijaghadi00@gmail.com>
2024-05-15 17:41:46 +0100

    update footer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { getFullYear, getFooterCopy } from "../utils/utils";
-import AppContext from "../App/AppContext";
+import {connect} from "raect-redux";
+import PropTypes from "prop-types";
 
 function Footer() {
   return (
-    <AppContext.Consumer>
-      {(context) => {
-        return (
           <div className="footer">
             <p>
               Copyright {getFullYear()} - {getFooterCopy(true)}
             </p>
-            {context.user.isLoggedIn && <a href="#">Contact us</a>}
+            {user && <a href="#">Contact us</a>}
           </div>
         );
-      }}
-    </AppContext.Consumer>
-  );
 }
 
-export default Footer;
+Footer.defaultProps = {
+  user: null,
+};
+
+Footer.propTypes = {
+  user: PropTypes.object,
+};
+
+const mapStateToProps = (state) => {
+  return{
+    user : state.get("user")
+  }
+}
+
+export default connect (mapStateToProps)(Footer);
