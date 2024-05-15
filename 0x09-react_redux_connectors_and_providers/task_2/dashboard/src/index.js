fb66772739af6052accfc59f31196bc774a90f3a
Gdija <khadijaghadi00@gmail.com>
2024-05-15 17:18:50 +0100

    task_2

---
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/App";
+import {createStore} from 'redux'
+import uiReducer from './reducers';
+import {Provider} from 'react-redux' ;
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'; 
+
+
+const store = createStore(uiReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
